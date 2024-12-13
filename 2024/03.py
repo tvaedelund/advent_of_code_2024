bafab5496330eac183e76557edbed0bef00a5fe5
Tomas Vaedelund <tomas.vaedelund@heidelbergmaterials.com>
2024-12-13 10:11:54 +0100

    moved stuff

--- 2024/03.py
+++ 2024/03.py
@@ -3,9 +3,9 @@ import re
 input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
-# with open('data/03_data.txt', 'r') as file:
-#     input = file.read()
-#     input = input.replace('\n', '')
+with open('data/03_data.txt', 'r') as file:
+    input = file.read()
+    input = input.replace('\n', '')
 
 def solve_1():
     pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
@@ -28,7 +28,7 @@ def solve_2():
             do = False
         else:
             if do:
-                x, y = match
+                x, y = re.findall(r'\d{1,3}', match)
                 result += int(x) * int(y)
     return result
 
