fc23e0a44303646e687236c998831d1d38a9b6c4
Tomas Vaedelund <tomas.vaedelund@heidelbergmaterials.com>
2024-12-13 09:58:10 +0100

    opti day 2024_01

--- 01.py
+++ 01.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 input = """3   4
 4   3
 2   5
@@ -22,18 +24,15 @@ column1 = sorted(column1)
 column2 = sorted(column2)
 
 def solve_1():
-    result = 0
-    for i in range(0, len(column1)):
-        result += abs(column1[i] - column2[i])
-    return result
+    return sum(abs(c1 - c2) for c1, c2 in zip(column1, column2))
 
 def solve_2():
-    result = []
-    for i in range(len(column1)):
-        for j in range(len(column2)):
-            if column1[i] == column2[j]:
-                result.append(column1[i])
-    return sum(result)
+    count_column2 = Counter(column2)
+    result = 0
+    for num in column1:
+        if num in count_column2:
+            result += num * count_column2[num]
+    return result
 
 print("Result 1: " + str(solve_1()))
 print("Result 2: " + str(solve_2()))
